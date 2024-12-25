6bb6129008edfbd839608250361293df8fc819e4
xiaodaxia <xiaozisheng2008@qq.com>
2024-12-25 15:12:47 +0800

    add example of how to display utf8 text

--- READMe.md
+++ READMe.md
@@ -7,3 +7,23 @@ This is a standalone version of coin3d dragger implemented in FreeCAD, therefore
 use vcpkg to install `soqt` package and build with cmake.
 
 ![example](./soqt-fcdragger.jpg)
+
+# Notes
+
+## How to display a utf8 text
+
+Generally you can use `SoText2` to display a utf8 text, but the default font only contains latin charset, therefore an extra font is required. Set the font with `SoFont` node, put the font ttf file in the runtime path or set the `COIN_FONT_PATH` environment variable (`coin_setenv` may be helpful). See [here](https://www.coin3d.org/coin/classSoFont.html#details) for more details. 
+
+```c++
+auto font = new SoFont;
+font->name = "NotoSansSC-Regular.ttf";
+
+auto text = new SoText2;
+text->string = "Alçapão, Hello, 你好！";
+
+auto sep = new SoSeparator;
+sep->addChild(font);
+sep->addChild(text);
+```
+
+![coin-utf8-text](./coin-utf8-text.jpg)
--- dragger.cpp
+++ dragger.cpp
@@ -2,18 +2,31 @@
 #include <Inventor/Qt/SoQt.h>
 #include <Inventor/Qt/viewers/SoQtExaminerViewer.h>
 #include <Inventor/draggers/SoTransformBoxDragger.h>
+#include <Inventor/nodes/SoFont.h>
 #include <Inventor/nodes/SoSeparator.h>
+#include <Inventor/nodes/SoText3.h>
 #include <spdlog/spdlog.h>
 
 int main(int argc, char **argv) {
+  coin_setenv("COIN_FONT_PATH", "C:/AppSource/coin-examples", 1);
+
   QWidget *window = SoQt::init(argv[0]);
   Gui::SoFCCSysDragger::initClass();
   Gui::So3DAnnotation::initClass();
+  auto sep = new SoSeparator;
 
   auto *dragger = new Gui::SoFCCSysDragger;
+  sep->addChild(dragger);
+  auto font = new SoFont;
+  font->name = "NotoSansSC-Regular.ttf";
+  font->size = 10;
+  auto text = new SoText3;
+  text->string = "Alçapão, Hello, 你好！";
+  sep->addChild(font);
+  sep->addChild(text);
 
   SoQtExaminerViewer *viewer = new SoQtExaminerViewer(window);
-  viewer->setSceneGraph(dragger);
+  viewer->setSceneGraph(sep);
   viewer->show();
   SoQt::show(window);
 
