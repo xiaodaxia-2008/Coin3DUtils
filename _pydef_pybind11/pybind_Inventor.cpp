f048b358fa0e983bb99da1f7a8fb6a190a0a10f9
xiaodaxia <xiaozisheng2008@qq.com>
2025-04-15 13:05:02 +0800

    refactor

--- _pydef_pybind11/pybind_Inventor.cpp
+++
@@ -1,8 +0,0 @@
-#include <pybind11/pybind11.h>
-#include <pybind11/stl.h>
-#include <pybind11/functional.h>
-#include <pybind11/numpy.h>
-
-#include <Inventor/nodes/SoSeparator.h>
-
-namespace py = pybind11;
