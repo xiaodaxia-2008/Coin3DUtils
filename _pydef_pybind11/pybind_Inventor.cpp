998eccc457a0c5ea56faf8ce862b7e4f3ea3b41b
xiaodaxia <xiaozisheng2008@qq.com>
2025-04-15 12:05:25 +0800

    experimental litgen

---
+++ _pydef_pybind11/pybind_Inventor.cpp
@@ -0,0 +1,8 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <pybind11/functional.h>
+#include <pybind11/numpy.h>
+
+#include <Inventor/nodes/SoSeparator.h>
+
+namespace py = pybind11;
---
+++ tools/generate_bindings.py
@@ -0,0 +1,149 @@
+import litgen
+import os
+import glob
+
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+
+if "LITGEN_USE_NANOBIND" in os.environ and os.environ["LITGEN_USE_NANOBIND"] == "ON":
+    LITGEN_USE_NANOBIND = True
+else:
+    LITGEN_USE_NANOBIND = False
+
+
+def my_litgen_options() -> litgen.LitgenOptions:
+    # configure your options here
+    options = litgen.LitgenOptions()
+
+    if LITGEN_USE_NANOBIND:
+        options.bind_library = litgen.BindLibraryType.nanobind
+
+    # ///////////////////////////////////////////////////////////////////
+    #  Root namespace
+    # ///////////////////////////////////////////////////////////////////
+    # The namespace DaftLib is the C++ root namespace for the generated bindings
+    # (i.e. no submodule will be generated for it in the python bindings)
+    options.namespaces_root = ["PyInventor"]
+
+    # //////////////////////////////////////////////////////////////////
+    # Basic functions bindings
+    # ////////////////////////////////////////////////////////////////////
+    # No specific option is needed for these basic bindings
+    # litgen will add the docstrings automatically in the python bindings
+
+    # //////////////////////////////////////////////////////////////////
+    # Classes and structs bindings
+    # //////////////////////////////////////////////////////////////////
+    # No specific option is needed for these bindings.
+    # - Litgen will automatically add a default constructor with named parameters
+    #   for structs that have no constructor defined in C++.
+    #  - A class will publish only its public methods and members
+
+    # ///////////////////////////////////////////////////////////////////
+    #  Exclude functions and/or parameters from the bindings
+    # ///////////////////////////////////////////////////////////////////
+    # We want to exclude `inline void priv_SetOptions(bool v) {}` from the bindings
+    # priv_ is a prefix for private functions that we don't want to expose
+    options.fn_exclude_by_name__regex = "^priv_"
+
+    # Inside `inline void SetOptions(bool v, bool priv_param = false) {}`,
+    # we don't want to expose the private parameter priv_param
+    # (it is possible since it has a default value)
+    options.fn_params_exclude_names__regex = "^priv_"
+
+    # ////////////////////////////////////////////////////////////////////
+    # Override virtual methods in python
+    # ////////////////////////////////////////////////////////////////////
+    # The virtual methods of this class can be overriden in python
+    options.class_override_virtual_methods_in_python__regex = "^Animal$"
+
+    # ////////////////////////////////////////////////////////////////////
+    # Publish bindings for template functions
+    # ////////////////////////////////////////////////////////////////////
+    #  template<typename T> T MaxValue(const std::vector<T>& values);
+    # will be published as: max_value_int and max_value_float
+    # options.fn_template_options.add_specialization("^MaxValue$", ["int", "float"], add_suffix_to_function_name=True)
+    #  template<typename T> T MaxValue(const std::vector<T>& values);
+    # will be published as: max_value_int and max_value_float
+    # options.fn_template_options.add_specialization("^MinValue$", ["int", "float"], add_suffix_to_function_name=False)
+
+    # ////////////////////////////////////////////////////////////////////
+    # Return values policy
+    # ////////////////////////////////////////////////////////////////////
+    # `Widget& GetWidgetSingleton()` returns a reference, that python should not free,
+    # so we force the reference policy to be 'reference' instead of 'automatic'
+    # options.fn_return_force_policy_reference_for_references__regex = "Singleton$"
+
+    # ////////////////////////////////////////////////////////////////////
+    #  Boxed types
+    # ////////////////////////////////////////////////////////////////////
+    # Adaptation for `inline void SwitchBoolValue(bool &v) { v = !v; }`
+    # SwitchBoolValue is a C++ function that takes a bool parameter by reference and changes its value
+    # Since bool are immutable in python, we can to use a BoxedBool instead
+    # options.fn_params_replace_modifiable_immutable_by_boxed__regex = "^SwitchBoolValue$"
+
+    # ////////////////////////////////////////////////////////////////////
+    #  Published vectorized math functions and namespaces
+    # ////////////////////////////////////////////////////////////////////
+    # The functions in the MathFunctions namespace will be also published as vectorized functions
+    # options.fn_namespace_vectorize__regex = r"^DaftLib::MathFunctions$"  # Do it in this namespace only
+    # options.fn_vectorize__regex = r".*"  # For all functions
+
+    # ////////////////////////////////////////////////////////////////////
+    # Format the python stubs with black
+    # ////////////////////////////////////////////////////////////////////
+    # Set to True if you want the stub file to be formatted with black
+    options.python_run_black_formatter = True
+
+    return options
+
+
+def autogenerate() -> None:
+    repository_dir = os.path.realpath(os.path.dirname(__file__) + "/../")
+
+    include_dir = repository_dir + "/build/vcpkg_installed/x64-windows/include/Inventor"
+    header_files = [include_dir + "/nodes/SoNode.h"]
+    header_files = glob.glob(include_dir + "/*.h", recursive=True)
+
+    logger.debug(f"header fiels: {'\n'.join(header_files)}")
+
+    output_cpp_pydef_file = repository_dir + "/_pydef_pybind11/pybind_Inventor.cpp"
+    with open(output_cpp_pydef_file, "a") as f:
+        f.write(
+            """
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  AUTOGENERATED CODE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// <litgen_glue_code>  // Autogenerated code below! Do not edit!
+// </litgen_glue_code> // Autogenerated code end
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  AUTOGENERATED CODE END !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  AUTOGENERATED CODE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// <litgen_pydef>  // Autogenerated code below! Do not edit!
+// </litgen_pydef> // Autogenerated code end
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  AUTOGENERATED CODE END !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+"""
+        )
+
+    output_stub_pyi_file = repository_dir + "/_stubs/PyInventor/__init__.pyi"
+    with open(output_stub_pyi_file, "a") as f:
+        f.write(
+            """
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  AUTOGENERATED CODE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# <litgen_stub> // Autogenerated code below! Do not edit!
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  AUTOGENERATED CODE END !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# </litgen_stub> // Autogenerated code end!
+"""
+        )
+
+    litgen.write_generated_code_for_files(
+        options=my_litgen_options(),
+        input_cpp_header_files=header_files,
+        output_cpp_pydef_file=output_cpp_pydef_file,
+        output_stub_pyi_file=output_stub_pyi_file,
+    )
+
+
+if __name__ == "__main__":
+    autogenerate()
